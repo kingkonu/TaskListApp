b3bae1a0f7e3812989a759b0f3efca9c7bd76a78
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-05-21 23:09:11 +0300

    Delete Function

--- TaskListApp/App/SceneDelegate.swift
+++ TaskListApp/App/SceneDelegate.swift
@@ -11,6 +11,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    private let storageManager = StorageManager.shared
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
@@ -19,8 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
+        storageManager.saveContext()
     }
-
-
 }
 
--- TaskListApp/ViewControllers/TaskListViewController.swift
+++ TaskListApp/ViewControllers/TaskListViewController.swift
@@ -36,6 +36,10 @@ final class TaskListViewController: UITableViewController {
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             let commit = taskList[indexPath.row]
